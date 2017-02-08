a1df2eb5a38aba20db725dc42ab449bd6d3da093
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-02-08 10:47:00 +0100

    change the app url schema from http to https

--- heroku-start.sh
+++ heroku-start.sh
@@ -3,7 +3,7 @@
 env
 
 if [ "$SERVICE_NAME" == "topmenubar" ]; then
-    java $JAVA_OPTS -jar $SERVICE_NAME/target/$SERVICE_NAME*.jar --server.port=$PORT --applauncher.url=http://tmb-applauncher.herokuapp.com/applauncher
+    java $JAVA_OPTS -jar $SERVICE_NAME/target/$SERVICE_NAME*.jar --server.port=$PORT --applauncher.url=https://tmb-applauncher.herokuapp.com/applauncher
 else
     java $JAVA_OPTS -jar $SERVICE_NAME/target/$SERVICE_NAME*.jar --server.port=$PORT
 fi
