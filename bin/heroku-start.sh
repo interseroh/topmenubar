29b42f3895552352725eb811198107e6dd715b4a
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-02-08 15:14:57 +0100

    WIP tries to extract the pull request number

--- bin/heroku-start.sh
+++ bin/heroku-start.sh
@@ -2,7 +2,11 @@
 
 env
 
+
 if [ "$SERVICE_NAME" == "topmenubar" ]; then
+
+    PULL_REQUEST_NUMBER = echo $HEROKU_NAME | sed "s/[a-zA-Z\\-]//g"
+    echo "PullRequest: $PULL_REQUEST_NUMBER"
     java $JAVA_OPTS -jar $SERVICE_NAME/target/$SERVICE_NAME*.jar --server.port=$PORT --applauncher.url=https://tmb-applauncher.herokuapp.com/applauncher
 else
     java $JAVA_OPTS -jar $SERVICE_NAME/target/$SERVICE_NAME*.jar --server.port=$PORT
