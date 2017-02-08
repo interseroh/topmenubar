2f22185ef7b90e6e0a81d352f6af7c7ec41b3eed
DESKTOP-N7D3P3D\alexadmin <oleksandr.peognimak@crowdcode.io>
2017-02-08 08:44:59 +0100

    [TPI-49]; Add  License agreenemt and Log4j logger

--- topmenubar/src/main/java/de/interseroh/tmb/client/common/RemoteScriptInjector.java
+++ topmenubar/src/main/java/de/interseroh/tmb/client/common/RemoteScriptInjector.java
@@ -1,33 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package de.interseroh.tmb.client.common;
 
 import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.ScriptInjector;
-import com.google.gwt.user.client.Window;
+import org.apache.log4j.Logger;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- * Injects JavaScript  from remote server
- */
 public class RemoteScriptInjector {
 	private static final Logger logger = Logger
 			.getLogger(RemoteScriptInjector.class.getName());
 
 	public void injectScript(String applicationUrl, String scriptPath) {
 		String scriptFullUrl = applicationUrl + scriptPath;
-		logger.info("Start JavaScript injecting  from URL :" + scriptFullUrl);
+		logger.debug("Start JavaScript injecting  from URL :" + scriptFullUrl);
 		ScriptInjector.fromUrl(scriptFullUrl).setCallback(new Callback() {
 			@Override
 			public void onFailure(Object o) {
 
-				logger.info("Error: Can not load JavaScript from from URL :"
+				logger.debug("Error: Can not load JavaScript from from URL :"
 						+ scriptFullUrl);
 			}
 
 			@Override
 			public void onSuccess(Object o) {
-				logger.info("Java Script is lodaed from URL :" + scriptFullUrl);
+				logger.error("Java Script is lodaed from URL :" + scriptFullUrl);
 			}
 		}).setWindow(ScriptInjector.TOP_WINDOW).inject();
 	}
