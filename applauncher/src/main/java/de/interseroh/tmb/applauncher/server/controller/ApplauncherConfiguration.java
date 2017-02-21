4a2c0b000d3afa8f256aeecf3061b04c41543544
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-02-21 11:44:15 +0100

    remove @CrossOrigin Annotation and prettify code.

--- applauncher/src/main/java/de/interseroh/tmb/applauncher/server/controller/ApplauncherConfiguration.java
+++ applauncher/src/main/java/de/interseroh/tmb/applauncher/server/controller/ApplauncherConfiguration.java
@@ -18,48 +18,45 @@
  */
 package de.interseroh.tmb.applauncher.server.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.interseroh.tmb.applauncher.shared.ApplauncherServiceEndpoint;
+import de.interseroh.tmb.applauncher.shared.json.ApplauncherProperties;
+import de.interseroh.tmb.applauncher.shared.json.TargetedApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import de.interseroh.tmb.applauncher.shared.ApplauncherServiceEndpoint;
-import de.interseroh.tmb.applauncher.shared.json.ApplauncherProperties;
-import de.interseroh.tmb.applauncher.shared.json.TargetedApplication;
-
 @RestController
-@CrossOrigin
 public class ApplauncherConfiguration {
-    private static final Logger logger = LoggerFactory
-            .getLogger(ApplauncherConfiguration.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(ApplauncherConfiguration.class);
 
-    @Value("${applauncher.config.json:classpath:dev.applauncher.json}")
-    private Resource applauncherConfigurationJson;
+	@Value("${applauncher.config.json:classpath:dev.applauncher.json}")
+	private Resource applauncherConfigurationJson;
 
-    @RequestMapping(value = ApplauncherServiceEndpoint.APPLAUNCHER_CONFIG, method = RequestMethod.GET)
-    public List<TargetedApplication> getConfiguration() {
-        List<TargetedApplication> listAppProps = new ArrayList<>();
-        try (InputStream jsonIs = applauncherConfigurationJson
-                .getInputStream()) {
-            ObjectMapper mapper = new ObjectMapper();
-            ApplauncherProperties appPropes = mapper.readValue(jsonIs, ApplauncherProperties.class);
-            listAppProps.addAll(appPropes.getApplauncherProperties()
-                    .getTargetedApplication());
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
+	@RequestMapping(value = ApplauncherServiceEndpoint.APPLAUNCHER_CONFIG, method = RequestMethod.GET)
+	public List<TargetedApplication> getConfiguration() {
+		List<TargetedApplication> listAppProps = new ArrayList<>();
+		try (InputStream jsonIs = applauncherConfigurationJson
+				.getInputStream()) {
+			ObjectMapper mapper = new ObjectMapper();
+			ApplauncherProperties appPropes = mapper
+					.readValue(jsonIs, ApplauncherProperties.class);
+			listAppProps.addAll(appPropes.getApplauncherProperties()
+					.getTargetedApplication());
+		} catch (IOException e) {
+			logger.error(e.getMessage(), e);
+		}
 
-        return listAppProps;
-    }
+		return listAppProps;
+	}
 }
